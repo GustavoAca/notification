62b1d0e4b0c2be74c6e625bde2f2e0f2ce2cc6f9
Gustavo Acácio <gustavo.acacio@attus.ai>
2025-04-30 22:59:56 -0300

    fix:removendo script desnecessario

--- src/test/java/com/glaiss/notification/NotificationApplicationTests.java
+++ src/test/java/com/glaiss/notification/NotificationApplicationTests.java
@@ -1,13 +1,8 @@
 package com.glaiss.notification;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class NotificationApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
 
 }
